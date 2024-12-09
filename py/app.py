63167ca284fc1beedb842d6263071f2e385339ce
Manolowo <diego001vm@gmail.com>
2024-12-09 15:06:06 -0300

    Acutalización
    Implementación de ambos modelos en conjunto. Modificación ligera a los estilos y resultados analíticos de los modelos.

--- py/app.py
+++ py/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pickle
+import joblib
 
 app = Flask(__name__)
 
@@ -11,24 +12,35 @@ def home():
 with open('../models/regresion/linear_regression_model.pkl', 'rb') as file:
     regression_model = pickle.load(file)
 
+model_svm = joblib.load('../models/clasification/SVM/svm_model.pkl')
+scaler = joblib.load('../models/clasification/SVM/scaler.pkl')
+
 @app.route('/regresion_prediction')
 def regresion_prediction():
-    return render_template('regresion_prediction.html', prediction=None)
+    return render_template('regresion_prediction.html', prediction1=None, prediction_svm = None)
 
-@app.route('/predict_regresion', methods=['POST'])
-def predict1():
-    # Obtener valores del formulario
-    cuentas = float(request.form['cuentas'])
-    dualidad = float(request.form['dualidad'])
-    col_media = float(request.form['col_media'])
-    eeccnac_media = float(request.form['eeccnac_media'])
-
-    # Realizar la predicción
-    input_data = [[cuentas, dualidad, col_media, eeccnac_media]]
-    prediction = regression_model.predict(input_data)[0]
-
-    # Renderizar la página con el resultado
-    return render_template('regresion_prediction.html', prediction=round(prediction, 2))
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    prediction1 = None
+    prediction_svm = None
+    
+    if request.method == 'POST':
+        # Obtener los valores del formulario
+        cuentas = int(request.form['cuentas'])
+        dualidad = int(request.form['dualidad'])
+        col_media = float(request.form['col_media'])
+        eeccnac_media = float(request.form['eeccnac_media'])
+        
+        # Realizar la predicción
+        input_data = [[cuentas, dualidad, col_media, eeccnac_media]]
+        prediction1 = regression_model.predict(input_data)[0]
+        
+        # Predicción con el modelo SVM
+        data = [[cuentas, col_media, eeccnac_media, dualidad]]
+        data_scaled = scaler.transform(data)
+        prediction_svm = model_svm.predict(data_scaled)[0]
+        
+    return render_template('regresion_prediction.html', prediction1=prediction1, prediction_svm=prediction_svm)
 
 if __name__ == '__main__':
     app.run(debug=True)
